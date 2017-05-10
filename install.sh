9c2e1b48c0d6927bbfe92c8645ed820e91083aa6
Ray Kootstra <ray@webserver.home>
2017-05-10 16:37:37 +0200

    Added all docker files

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ln -s ./vendor/stackinstance/docker-centos/docker ./
-cp -r vendor/stackinstance/docker-centos/docker/php-apache/Dockerfile ./.
+cp -r vendor/stackinstance/docker-centos/docker/docker-compose.yml ./.
