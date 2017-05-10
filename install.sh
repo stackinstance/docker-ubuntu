66afba22396c24d0d9a2f3bad00c610f3069c527
Ray Kootstra <ray@webserver.home>
2017-05-10 16:48:21 +0200

    Fixed install script part 2

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ln -s ./vendor/stackinstance/docker-centos/docker ./
-cp -r ./vendor/stackinstance/docker-centos/docker-compose.yml ./.
+ln -s ./vendor/stackinstance/docker-centos/docker-compose.yml ./
+#cp -r ./vendor/stackinstance/docker-centos/docker-compose.yml ./.
