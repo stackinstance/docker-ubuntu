fde9c5c95071b9112f7ee2ae238fa7843f8058f2
Ray Kootstra <ray@webserver.home>
2017-05-10 20:38:48 +0200

    Updated rebuild shell script

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Step 1: run command: composer require stackinstance/docker-centos:dev-master --dev
 
 Step 2: run command: vendor/stackinstance/docker-centos/install.sh
+
+Step 3: Open docker terminal and run command: vendor/stackinstance/docker-centos/docker/tools/rebuild.sh
+
--- docker/tools/rebuild.sh
+++ docker/tools/rebuild.sh
@@ -3,4 +3,6 @@
 docker-compose --project-name project down
 docker rmi project_php-apache
 docker-compose --project-name project build --no-cache
-docker-compose --project-name project up
+docker-compose --project-name project up -d
+
+docker exec -t -i -u www-data project_php-apache_1 /bin/bash -c 'cd /var/www ; exec bash'
