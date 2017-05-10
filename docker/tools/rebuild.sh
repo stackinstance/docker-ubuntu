02b45b8477bad9c7fe0ac858ba408a57053db08f
Ray Kootstra <ray@webserver.home>
2017-05-10 20:47:52 +0200

    Updated docker file (removed ssh key copy) and fixed rebuild shell script

--- docker/tools/rebuild.sh
+++ docker/tools/rebuild.sh
@@ -5,4 +5,4 @@ docker rmi project_php-apache
 docker-compose --project-name project build --no-cache
 docker-compose --project-name project up -d
 
-docker exec -t -i -u www-data project_php-apache_1 /bin/bash -c 'cd /var/www ; exec bash'
+docker exec -t -i -u apache project_php-apache_1 /bin/bash -c 'cd /var/www ; exec bash'
