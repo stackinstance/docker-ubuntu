f35c8b0c60750c5f516559f4ff7b7babac9889cd
Stack Instance <ray@kootstra.net>
2017-05-11 12:44:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./web
-ln -s ./vendor/stackinstance/docker-centos/docker ./
-ln -s ./vendor/stackinstance/docker-centos/docker-compose.yml ./
+ln -s ./vendor/stackinstance/docker-ubuntu/docker ./
+ln -s ./vendor/stackinstance/docker-ubuntu/docker-compose.yml ./
