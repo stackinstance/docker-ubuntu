7e9f3e9e26a9a3c27d19c441dbfa2ae0f416bcca
Ray Kootstra <ray@webserver.home>
2017-05-11 10:51:03 +0200

    Added create web folder if not exists

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+mkdir -p ./web
 ln -s ./vendor/stackinstance/docker-centos/docker ./
 ln -s ./vendor/stackinstance/docker-centos/docker-compose.yml ./
-#cp -r ./vendor/stackinstance/docker-centos/docker-compose.yml ./.
